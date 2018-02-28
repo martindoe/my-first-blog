f508f66bf0d32b4bcf18570a77b5cdc6f9d5c70f
martindoe <36802467+martindoe@users.noreply.github.com>
2018-02-28 14:05:06 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cjlc^*8ow-n+jg^c3la%yu&y9s_u-90v!jzt)-1w7edcy7^5#j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'martindoe.pythonanywhere.com']
+ALLOWED_HOSTS = ['https//', 'martindoe.pythonanywhere.com']
 
 
 # Application definition
